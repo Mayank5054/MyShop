2ee9f698574ae0f0759dfaed7a4a0595cfc966dd
Mayank5054 <mayanksheladiya49@gmail.com>
2023-12-21 18:49:04 +0530

    Utils added for path

--- router/Admin.js
+++ router/Admin.js
@@ -1,19 +1,21 @@
 const express=require("express");
 
+const path=require("path");
+
 const router=express.Router();
 
+const root=require("../utils/path");
 router.use("/add-product",(req,res,next)=>{
-    res.send(`
-    <form action="/admin/product" method="post">
-    <input type="text" name="title">
-    <input type="submit">
-    </form>
-    `);
+ res.sendFile(path.join(root,"views","add-product.html"));
 })
 
-router.use("/product",(req,res,next)=>{
+router.use("/all-product",(req,res,next)=>{
     console.log(req.body);
-    res.send("Product added");
+    res.send(`<h1>${req.body.title}</h1>`);
 })
+
+router.use("/",(req,res,next)=>{
+    res.sendFile(path.join(root,"views","shop.html"));
+    });
 
 module.exports=router;
