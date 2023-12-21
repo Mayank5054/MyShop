6fb9a8968729572e48a072ea871fe47ce7ca7920
Mayank5054 <mayanksheladiya49@gmail.com>
2023-12-21 11:29:54 +0530

    node project initiated

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
-//  if we ommit ./ here then it is serach module globally otherwise 
-// on the local 
-const http=require("http");
-
+const  http=require("http");
 
 const server=http.createServer((req,res)=>{
-    console.log(req);
-});
+console.log("Request Recieved");
+})
 
 server.listen(5354);
