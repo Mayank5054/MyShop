b66a017f03672eeed90865bd91918939de9ad6db
Mayank5054 <mayanksheladiya49@gmail.com>
2023-12-25 01:01:42 +0530

    added data displayed on different page

--- contollers/Sequelize_Controllers/AdminContoller.js
+++ contollers/Sequelize_Controllers/AdminContoller.js
@@ -1,5 +1,5 @@
 
-const product = require("../../models_sequelize/product");
+
 const Product=require("../../models_sequelize/product");
 exports.addProduct = (req,res,next)=>{
 res.render("./Sequelize_views/add_product_form.ejs");
@@ -7,15 +7,24 @@ res.render("./Sequelize_views/add_product_form.ejs");
 
 
  exports.postProduct=(req,res,next)=>{
-  product.create({
+  Product.create({
     title:req.body.title,
     price:req.body.price
   }).then((e)=>{
 console.log("Data Added");
+res.redirect("/Sequelize/showAllProduct");
   })
-  res.redirect("/Sequelize/showAllProduct");
+  
  }
 
  exports.showAllProduct=(req,res,next)=>{
-    res.render("./Sequelize_views/show_product.ejs");
+    Product.findAll()
+    .then((e)=>{
+        console.log(e);
+        res.render("./Sequelize_views/show_product.ejs",{
+            products:e
+        });
+    })
+    .catch();
+    
  }
