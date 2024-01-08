9f107a0ef574d4fe7767fd867c7e8e0334c861ac
Mayank5054 <mayanksheladiya49@gmail.com>
2024-01-08 12:56:04 +0530

    delete product added

--- contollers/mongoose_contollers/Admin.js
+++ contollers/mongoose_contollers/Admin.js
@@ -1,4 +1,4 @@
-const Product = require("../../models/mongooseModels/Product");
+var Product = require("../../models/mongooseModels/Product");
 
 exports.addProduct = (req,res,next) =>{
     console.log("hello,world");
@@ -6,7 +6,14 @@ exports.addProduct = (req,res,next) =>{
 
 }
 
-
+exports.allProduct = (req,res,next) => {
+    Product.find()
+    .then(data => {
+        res.render("./Mongoose_views/showAllProduct.ejs",
+        {data:data});
+        // console.log(data);
+    })
+}
 exports.handleAddProduct = (req,res,next) =>{
 const name=req.body.name;
 const price=req.body.price;
@@ -30,4 +37,49 @@ product.save()
     // console.log("data added");
 })
 
+}
+
+
+exports.updateProduct = (req,res,next) => {
+    Product.findById(req.body.id)
+    .then(product => {
+        console.log("updated",product);
+       res.render("./Mongoose_views/updateProductTemp.ejs",{
+        title:product.title,
+        price:product.price,
+        id:product._id,
+       }) 
+    })
+}
+
+exports.finishUpdate = (req,res,next) =>{
+    console.log("finish UPdate called");
+    // console.log(req.body.id);
+    Product.findById(req.body.id)
+    .then(product => {
+        // console.log("product , ",product);
+        console.log(req.body.title);
+        console.log(req.body.price);
+        product.title=req.body.title;
+        product.price=req.body.price;
+        // console.log(product);
+        product.save()
+        .then(result => {console.log("Product Updated");console.log(result);
+        res.redirect("/mongoose/allProduct");
+    })
+        .catch(error => {console.log(error);})
+    })
+}
+
+exports.deleteProduct = (req,res,next) =>{
+    const id=req.body.id;
+   
+    Product.findByIdAndDelete(id)
+    .then(
+        result =>{
+            console.log("product deleted");
+            console.log(result);
+            res.redirect("/mongoose/allProduct");
+        }
+    )
 }
--- routes/mongoose_routes/Admin.js
+++ routes/mongoose_routes/Admin.js
@@ -3,7 +3,11 @@ const router=express.Router();
 const mongooseController=require("../../contollers/mongoose_contollers/Admin");
 
 router.get("/add",mongooseController.addProduct);
+router.get("/allProduct",mongooseController.allProduct);
 router.post("/addProductSubmit",mongooseController.handleAddProduct);
+router.post("/updateProductPage",mongooseController.updateProduct);
+router.use("/finishProductUpdate",mongooseController.finishUpdate);
+router.post("/deleteProduct",mongooseController.deleteProduct);
 // router.get("/showAllProduct",mongooseController.);
 
 module.exports=router;
